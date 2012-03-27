6b13894a9852f9d5159985f3823a018b3c3fbc5e
Ijon Tichy <gztichy@lavabit.com>
2012-03-27 12:39:49 -0500

    Changing up the monsters - zombieman, shotgun guy, and chaingun guy are Quakeified as of right now

--- packagepk3.sh
+++
@@ -1,1 +0,0 @@
-/home/ijontichy/projects/packagepk3.sh
--- pk3/acs/instantswitch.c
+++ pk3/acs/instantswitch.c
@@ -12,9 +12,14 @@ function int sqrt(int number)
   return val;
 }
 
+function int percFloat(int intg, int frac, int frac1000)
+{
+    return (intg << 16) + ((frac << 16) / 100) + ((frac1000 << 16) / 10000);
+}
+
 script 583 (int x, int y, int z)
 {
-    SetResultValue(sqrt(x*X + y*y + z*z) );
+    SetResultValue(sqrt(x*x + y*y + z*z) );
 }
 
 script 584 (int take)
@@ -28,4 +33,26 @@ script 584 (int take)
     {
         TakeInventory("QuakeCell", ret);
     }
+}
+
+script 585 (int divI, int divF, int divF1)
+{
+    int div = percFloat(divI, divF, divF1);
+ 
+    int x = GetActorVelX(0);
+    int y = GetActorVelY(0);
+    int z = GetActorVelZ(0);
+
+    SetActorVelocity(0, FixedDiv(x, div), FixedDiv(y, div), FixedDiv(z, div), 0, 1);
+}
+
+script 586 (int divI, int divF, int divF1)
+{
+    int div = percFloat(divI, divF, divF1);
+
+    int x = GetActorVelX(0);
+    int y = GetActorVelY(0);
+    int z = GetActorVelZ(0);
+
+    SetActorVelocity(0, FixedMul(x, div), FixedMul(y, div), FixedMul(z, div), 0, 1);
 }
